621c36d3e4512c3c80721ffeff5ddaba4f29ca73
Egor <123231130+Darkness-Warrior@users.noreply.github.com>
2023-05-07 03:30:37 +0300

    Add files via upload

---
+++ Task_6(V.2).py
@@ -0,0 +1,21 @@
+def sizer(cls):
+    class SizedClass(cls):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            try:
+                self.size = len(self)
+            except TypeError:
+                try:
+                    self.size = abs(self)
+                except TypeError:
+                    self.size = 0
+    print("Оно живое!")
+    return SizedClass
+
+
+@sizer
+class MyClass:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
